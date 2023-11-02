cd7b3f2bf3496894f7faed9202b760a8aaa6028c
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-02 16:31:33 +0100

    feat(app.module): add AccueilComponent MesPaiementsComponent and MesInformationtsComponen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { MesPaiementsComponent } from './mes-paiements/mes-paiements.component';
+import { MesInformationtsComponent } from './mes-informationts/mes-informationts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    MesPaiementsComponent,
+    MesInformationtsComponent
   ],
   imports: [
     BrowserModule,
