a22b4c32c239705c6ba05f0a92cbe96922e5b43f
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-03 15:43:08 +0100

    feat(app.module): declare http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { MesPaiementsComponent } from './mes-paiements/mes-paiements.component';
@@ -16,7 +18,8 @@ import { MesInformationtsComponent } from './mes-informationts/mes-informationts
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
