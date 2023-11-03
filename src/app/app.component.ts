193e07866ff9ec21339a55c81327a28be6ced2a2
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-03 16:09:52 +0100

    feat(app.component): display properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'authentication-angular';
-  greeting: {} = {'id': 'XXX', 'content': 'Hello World'} ;
+  greeting: any = {'id': 'XXX', 'content': 'Hello World'} ;
 
   constructor(private http: HttpClient) {
     http.get('resource').subscribe(data => this.greeting = data);
