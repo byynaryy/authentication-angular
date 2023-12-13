c6006d048bc305feeca4a3bbfbeb7c965c003fb7
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-12-13 14:09:13 +0100

    feat(app-routing.module.ts): add login path and redirect empty path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { AppComponent } from './app.component';
 import { MesInformationtsComponent } from './mes-informationts/mes-informationts.component';
 import { MesPaiementsComponent } from './mes-paiements/mes-paiements.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-    {path: "", component: AccueilComponent},
+    
+    { path: '', pathMatch: 'full', redirectTo: 'home'},
+    {path: "home", component: AccueilComponent},
     {path: "informations", component: MesInformationtsComponent},
     {path: "paiements", component: MesPaiementsComponent},
+    {path: "login", component: LoginComponent }
 ];
 
 @NgModule({
